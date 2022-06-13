217e90bdc1852d3a2c25a169740409cc31091db1
Zachary Soliman <zachsoliman1@gmail.com>
2022-06-12 21:31:39 -0400

    All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(arr) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        let message = `Thank you, ${arr[i]}, for the wonderful surprise gift!`
+
+        newArr.push(message)
+        // create thank you message
+        // add thank you message to new arr
+    }
+
+    return newArr
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
+
